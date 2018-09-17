a977b04534638b924e2b57f55fdd6579a23bf15c
Nuno Fidalgo <nunorfidalgo@gmail.com>
2018-09-17 22:13:41 +0100

    Add project files.

---
+++ ex4/CaixaCanetas.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex4
+{
+    class CaixaCanetas
+    {
+        private int capacidade;
+        private double preco;
+        private string modelo;
+
+        public CaixaCanetas(int cap, double prec, string mod)
+        {
+            capacidade = cap;
+            preco = prec;
+            modelo = mod;
+        }
+
+        public CaixaCanetas()
+        {
+            Console.WriteLine("Object is being created");
+        }
+        ~CaixaCanetas()
+        {   //destructor
+            Console.WriteLine("Object is being deleted");
+        }
+
+        public void setCapacidade(int cap)
+        {
+            capacidade = cap;
+        }
+
+        public int getCapacidade()
+        {
+            return capacidade;
+        }
+
+        public void setPreco(double pre)
+        {
+            preco = pre;
+        }
+
+        public double getPreco()
+        {
+            return preco;
+        }
+
+        public void setModelo(string str)
+        {
+            modelo = str;
+        }
+
+        public string getModelo()
+        {
+            return modelo;
+        }
+
+
+
+        public int calcNumCaixas(int num_canetas)
+        {
+            return capacidade * num_canetas;
+        }
+    }
+}
---
+++ ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //CaixaCanetas tipo = new CaixaCanetas();
+            //tipo.setCapacidade(1);
+
+            //Console.WriteLine("teste: {0}", tipo.getCapacidade());
+
+            CaixaCanetas canetas = new CaixaCanetas(10, 2, "bic");
+
+            Console.WriteLine("capacidade: {0}", canetas.getCapacidade());
+            Console.WriteLine("preço: {0}", canetas.getPreco());
+            Console.WriteLine("preço: {0}", canetas.getModelo());
+
+
+        }
+    }
+}
